6851304dcc9ed08a058b8950fc99a4fabc7da3f7
Hamilton Mercado Cuellar <hamiltonmercadocuellar@iMacdeHamilton.home>
2020-11-22 17:11:40 +0100

    Contagiados

--- www/js/src.js
+++ www/js/src.js
@@ -27,3 +27,54 @@
 // console.log(past(0, 1, 1))
 
 // users
+
+/*
+  Quiero hacer una funcion que admita de parametro un array de pacientes
+  La funcion devuelve los que estan infectados al dia siguiente
+  Pero no no modifica los que estaban infectados originalmente
+  Si alguien esta infectado, al dia siguiente estan infectados los que estan justo al lado
+
+  Otro virus
+  Infecta igual que el anterior,
+  la diferencia es que el que estaba inicialmente infectado al dia siguiente esta sano
+  */
+
+const patients = [true, false, true, false, false, false, true, true];
+const infected = [true, true, true, true, false, true, true, true];
+
+const infected2 = [false, true, false, true, false, true, false, false];
+
+function comprobarSiHayElemento(arr, index, contagiado) {
+  if (arr[index] !== undefined) {
+    arr[index] = contagiado;
+  }
+}
+
+function consultaMedica(arr) {
+  let resultado = [...arr];
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index]) {
+      resultado[index] = true;
+      comprobarSiHayElemento(resultado, index - 1, true);
+      comprobarSiHayElemento(resultado, index + 1, true);
+    }
+  }
+  return resultado;
+}
+
+function consultaMedica2(arr) {
+  let resultado = [...arr];
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index]) {
+      resultado[index] = false;
+    } else if (arr[index] === arr[index + 1] && arr[index] === arr[index - 1]) {
+       resultado[index] = false;
+    } else {
+      resultado[index] = true;
+    }
+  }
+  return resultado;
+}
+console.log(patients);
+console.log(consultaMedica(patients));
+console.log(consultaMedica2(patients));
