a3099796d1754d9bcd1288c5ea9ac319367d10be
Hamilton Mercado Cuellar <hamiltonmercadocuellar@iMacdeHamilton.home>
2020-11-24 11:23:52 +0100

    Matrix

--- www/js/src.js
+++ www/js/src.js
@@ -109,7 +109,31 @@ function matrix(num) {
       resultado[a][b] = '*';
     }
   }
+  pintarDiagonales(resultado, 'H');
   return resultado;
 }
 
-console.log(matrix(3));
+function pintarDiagonales(arr, simbolo) {
+  let menos = arr.length - 1;
+  for (let index = 0; index < arr.length; index++) {
+    arr[index][index] = simbolo;
+    arr[index][menos--] = simbolo;
+  }
+  return arr;
+}
+
+function matrixPersonalizada(num, simboloRelleno, simboloDiagonal) {
+  let resultado = [];
+  for (let a = 0; a < num; a++) {
+    resultado.push([]);
+    for (let b = 0; b < num; b++) {
+      resultado[a][b] = simboloRelleno;
+    }
+  }
+  pintarDiagonales(resultado, simboloDiagonal);
+  return resultado;
+}
+
+console.log(matrix(4));
+
+console.log(matrixPersonalizada(5, '💻', '🧯'));
