3f3de7901de506dd7cc6a53cbba0be91d5a3b9f7
Hamilton <hamelshm@gmail.com>
2020-12-05 13:31:05 +0100

    hour

--- digitalclock/www/js/main.js
+++ digitalclock/www/js/main.js
@@ -2,6 +2,21 @@
 // h2 Buenos Días
 
 // DIGITAL HOUR
+'use strict';
 
+const time = new Date();
 
+let hour = time.getHours();
+let minutes = time.getMinutes();
+let seconds = time.getSeconds();
 
+const h1 = document.querySelector('h1');
+const h2 = document.querySelector('h2');
+
+const insertTime = (hour, minutes, seconds) => {
+  return `${hour} ${minutes} ${seconds}`;
+};
+
+setInterval(() => {
+  h1.innerHTML = insertTime(hour, minutes, seconds);
+}, 1000);
