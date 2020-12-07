c2b3824c1a36b515e51c8d8aee6bb72c779e6ad0
Hamilton <hamelshm@gmail.com>
2020-12-07 21:27:34 +0100

    Temp

---
+++ temperaturas/www/js/main.js
@@ -0,0 +1,31 @@
+// Temp. menor que 20: fondo verde
+// Temp. entre 20 y 30: fondo naranja
+// Temp. mayor de 30: fondo rojo
+
+const temperaturas = [
+  {
+    city: "A Coruña",
+    min: 17,
+    max: 23,
+  },
+  {
+    city: "Ferrol",
+    min: 15,
+    max: 32,
+  },
+  {
+    city: "Lugo",
+    min: -20,
+    max: 31,
+  },
+  {
+    city: "Ourense",
+    min: 18,
+    max: 35,
+  },
+  {
+    city: "Pontevedra",
+    min: 18,
+    max: 29,
+  },
+];
