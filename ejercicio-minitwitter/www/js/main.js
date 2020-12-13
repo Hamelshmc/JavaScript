80059b9656c57128fb83f4db7bf343380517a0aa
Hamilton Mercado Cuellar~ <hamelshmc@gmail.com>
2020-12-13 11:41:30 +0100

    result

--- ejercicio-minitwitter/www/js/main.js
+++ ejercicio-minitwitter/www/js/main.js
@@ -3,34 +3,66 @@
 const button = document.querySelector('button');
 const listaTweets = document.getElementById('tweets');
 
+const recovereData = localStorage.getItem('tweets');
+let localStorageTweets = recovereData ? JSON.parse(recovereData) : [];
+let generadorID = 0;
+
+initTweets(localStorageTweets);
+
 button.addEventListener('click', (event) => {
   event.preventDefault();
+  let text = document.getElementById('tweet');
 
-  let date = new Date().toLocaleDateString();
+  if (text.value.length === 0 || text.value.length > 250) {
+    window.alert('Esto no vale');
+    return;
+  }
 
-  const tweet = document.createElement('li');
-
-  const buttonBorrarTweet = document.createElement('button');
-  buttonBorrarTweet.textContent = 'borrar';
-
-  const time = document.createElement('time');
-  time.innerHTML = `${date}`;
-
-  const footer = document.createElement('footer');
-
-  const text = document.getElementById('tweet').value;
-
-  listaTweets.appendChild(tweet);
-  tweet.textContent = text;
-
-  tweet.appendChild(footer);
-
-  footer.appendChild(time);
-  footer.appendChild(buttonBorrarTweet);
+  const newTweet = { content: text.value, date: buildDate(), id: generadorID++ };
+  localStorageTweets.push(newTweet);
+  localStorage.setItem('tweets', JSON.stringify(localStorageTweets));
+  listaTweets.prepend(createTweet(newTweet));
+  text.value = '';
 });
 
 listaTweets.addEventListener('click', (event) => {
   if (event.target.matches('button')) {
     event.target.closest('li').remove();
+    const li = event.target.closest('li');
+
+    li.dataset.id;
+    localStorageTweets = localStorageTweets.filter((tweet) => tweet.id !== +li.dataset.id);
+    localStorage.setItem('tweets', JSON.stringify(localStorageTweets));
+    li.remove();
   }
 });
+
+function createTweet({ content, date, id }) {
+  const tweet = document.createElement('li');
+  const buttonBorrarTweet = document.createElement('button');
+  buttonBorrarTweet.textContent = 'borrar';
+
+  const time = document.createElement('time');
+  time.textContent = date;
+
+  const footer = document.createElement('footer');
+
+  tweet.textContent = content;
+  tweet.dataset.id = id;
+
+  tweet.appendChild(footer);
+
+  footer.appendChild(time);
+  footer.appendChild(buttonBorrarTweet);
+
+  return tweet;
+}
+function buildDate() {
+  return new Date().toLocaleDateString();
+}
+
+function initTweets(tweets) {
+  for (const tweet of tweets) {
+    listaTweets.prepend(createTweet(tweet));
+  }
+}
